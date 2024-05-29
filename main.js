133d64f59fde602756c8047b4fcc9d0297ce708f
iambasilp <iambasilp@gmail.com>
2024-05-29 19:11:58 +0530

    upgraded by forEach best Practice

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const Buttons = document.querySelectorAll(".btn");
+
 let digit = document.querySelector(".digit");
-
 let count = 0;
-
 
 Buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let styles = e.currentTarget.classList;
     if (styles.contains("increase")) {
       count++;
+    } else if (styles.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
     }
-    else if(styles.contains("decrease")){
-        count--;
-    }
-    else {
-        count = 0;
+
+    digit.textContent = count;
+    if (count < 0) {
+      digit.style.color = "red";
+    } else if (count > 0) {
+      digit.style.color = "lightgreen";
+    } else {
+      digit.style.color = "white";
     }
   });
 });
